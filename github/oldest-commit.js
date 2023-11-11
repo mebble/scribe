1291051f44fcbb4de35bc7c7ea2eec16d67c2246
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2023-11-11 22:47:12 +0530

    github oldest commit

---
+++ github/oldest-commit.js
@@ -0,0 +1,17 @@
+// credit to: https://github.com/bpceee/oldest
+const firstCommitUrl = (username, reponame) => {
+    const repo = `${username}/${reponame}`
+    const branchElement = document.querySelector("#branch-select-menu > summary > span.css-truncate-target");
+    const branch = branchElement ? branchElement.textContent : 'master'
+    console.log('wait for it...')
+    fetch(`https://github.com/${repo}/tree/${branch}`)
+        .then(res => res.text())
+        .then(res => {
+            const mainDocument = new DOMParser().parseFromString(res, 'text/html');
+            let commitCount = mainDocument.evaluate('//span[@class="d-none d-sm-inline"]//strong', mainDocument.body).iterateNext().innerText;
+            commitCount = Number(commitCount.trim().replaceAll(',', ''));
+            const commitId = mainDocument.evaluate('//*[@class="f6 Link--secondary text-mono ml-2 d-none d-lg-inline"]', mainDocument.body).iterateNext().getAttribute("href").split('/').pop();
+            const url = `https://github.com/${repo}/commits/${branch}?after=${commitId}+${commitCount - 10}`;
+            console.log(url)
+        })
+}
